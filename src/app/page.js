06717154945e1cf83ebdfd2d85bb622440ea3ec6
Rhomy <rhomy.prama@diginusantara.com>
2023-10-11 21:47:10 +0700

    tambah header comp

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,25 @@ import styles from './page.module.css'
 import Header from './../components/Header'
 
 export default function Home() {
+
+  const headerActions = [
+    {
+      title: 'About'
+    },
+    {
+      title: 'Features'
+    },
+    {
+      title: 'Pricing'
+    },
+    {
+      title: 'Testimonials'
+    },
+  ]
+
   return (
     <main className={styles.main}>
-       <Header   /> 
+       <Header actions={headerActions}  /> 
        
     oke
       
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,13 +6,26 @@ import Image from 'next/image';
 function Header({actions}) {
   return(
 
-    // logo 
-    <div>
+    <div className={styles.header}>
+      {/* logo  */}
       <div className={styles.logocontainer}>
         <Image width={40} height={40} src="/logo.png" alt={'logo'}/>
         <h2>Listicle</h2>
       </div>
+      
 
+      {/* actions  */}
+      <div className={styles.actionContainer}>
+        {actions.map(action => (
+          <h3 key={action.title} className={styles.action}>{action.title}</h3>    
+        ))}
+      </div>
+
+      {/* cta  */}
+      <div className={styles.cta}>
+        <h3>Sign In</h3>
+        <span><h3>Create an account</h3></span>
+      </div>
  
     
 
